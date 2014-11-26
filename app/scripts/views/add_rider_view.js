7cb032c50d7cc0e2f0f9c17632999dbd548ae578
Maggie Martin <maggierozay@gmail.com>
2014-11-26 14:26:16 -0500

    rider home and work lat and long are now stored

--- app/scripts/views/add_rider_view.js
+++ app/scripts/views/add_rider_view.js
@@ -19,55 +19,45 @@
     addRider: function (e) {
       e.preventDefault();
 
-      var geocoder = new google.maps.Geocoder();
-      var address = $('#rider_home').val();
-      var coordinates_obj;
-      var coordinates;
-
-       geocoder.geocode( { 'address': address}, function(results, status) {
-
-       if (status == google.maps.GeocoderStatus.OK) {
-         coordinates_obj = results[0].geometry.location;
-         coordinates = [coordinates_obj.k, coordinates_obj.B];
-
-         console.log(coordinates);
-
-           }
-         });
-
-      // var addy_home =  $('#rider_home').val();
-      //   window.getCoordinates(addy_home,
-      //       function(coordinates_home)
-      //       {
-      //         Data.homeCords = coordinates_home;
-      //
-      //       });
-      //
-      // var addy_work =  $('#rider_work').val();
-      // window.getCoordinates(addy_work,
-      //      function(coordinates_work)
-      //      {
-      //        Data.workCords = coordinates_work;
-      //        console.log(Data.workCords)
-      //      });
-
-
-
       var c = new App.Models.Rider({
         name: $('#rider_name').val(),
         home_address: $('#rider_home').val(),
-        work_address: $('#rider_work').val(),
-        home_latlong: coordinates
-
+        work_address: $('#rider_work').val()
       });
 
 
-      c.save(null, {
-        success: function () {
-          App.riders.add(c);
-          App.router.navigate('', { trigger: true });
+      var addy_home =  $('#rider_home').val();
+      var addy_work =  $('#rider_work').val();
+
+      // Get our home coordinates
+      window.getCoordinates(addy_home,
+        function(coordinates_home) {
+
+          // Set our home coordinates
+          c.set('home_latlong', coordinates_home);
+
+          // Get our work coordinates
+          window.getCoordinates(addy_work,
+            function(coordinates_work) {
+
+              // Set our work coordinates
+              c.set('work_latlong', coordinates_work);
+
+              // Save our entire object
+              c.save(null, {
+                success: function () {
+                  App.riders.add(c);
+                  App.router.navigate('', { trigger: true });
+                }
+            });
+          });
+
         }
-      });
+      );
+
+
+
+
 
     }
 
