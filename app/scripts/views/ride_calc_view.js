3ef5f6fab112be057369b30da61c70bc3956b979
Maggie Martin <maggierozay@gmail.com>
2014-12-02 21:16:15 -0500

    profiles are linked to on the matches view

--- app/scripts/views/ride_calc_view.js
+++ app/scripts/views/ride_calc_view.js
@@ -25,10 +25,33 @@
       //  var results2 = _.map(collection, function(other) {
       //   return currentUser.findDistance(other, 'work');
       //  });
+
+//       var container=$('.image-grid'),
+// item_title,
+// item_store,
+// item_price,
+// display;
+//
+//   etsyList.results.forEach( function (object) {
+//
+//   item_title = "<h3>" + object.title + "</h3>";
+//
+//   item_store = "<p>" + object.Shop.shop_name; + "</p>";
+//
+//   item_price = "<span>" + object.price + " " + object.currency_code + "<span>";
+//
+//   object.Images.forEach (function (y) {
+//   item_image = "<img src='" + y.url_170x135 + "' />";
+// });
+//
+//   display = "<li>" + item_image + item_title + item_store + item_price + "</li>";
+//   container.append(display);
+// });
       console.log(results);
+      //  <a href="#/allriders/<%= objectId %>">
       console.log('I live at ' + currentUser.attributes.home_address);
       var neighbors = _.each(results, function(x) {
-        $('.matchresults').append("<li>" + x.username + ' house ' + ' is ' + x.miles + ' miles away' + x.work + "</li>");
+        $('.matchresults').append("<li>" + "<a href='"+ '#/allriders/' + x.objectId +"' >"  + x.username + ' house ' + ' is ' + x.miles + ' miles away' + x.work +"</a>" + "</li>");
       });
 
 
