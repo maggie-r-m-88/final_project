63cec9d79007b0632f44fe3221bc41e7e354e788
Maggie Martin <maggierozay@gmail.com>
2014-12-03 01:56:38 -0500

    well i got it to work only thing that i dont have is sorting and having to keep pressing the button

--- app/scripts/rider_model.js
+++ app/scripts/rider_model.js
@@ -21,22 +21,32 @@
       //console.log(t + " has been added");
     },
 
-     findDistance: function (rider, location) {
+     findDistance: function (rider) {
 
      var  a, c, dm, dk, mi, km;
+     var  a1, c1, dm1, dk1, mi1, km1;
+
      var Rm = 3961; var Rk = 6373;
-     var coords_property;
+     //var coords_property;
 
-     if (location == 'home') {
-       coords_property = 'home_latlong';
-     } else {
-       coords_property = 'work_latlong';
-     }
+    //  if (location == 'home') {
+    //    coords_property = 'home_latlong';
+    //  } else {
+    //    coords_property = 'work_latlong';
+    //  }
+    var x2= rider.attributes.home_latlong[0];
+    var y2= rider.attributes.home_latlong[1];
+    var x1= currentUser.attributes.home_latlong[0];
+    var y1= currentUser.attributes.home_latlong[1];
 
-     var x2 = rider.get(coords_property)[0];
-     var y2 = rider.get(coords_property)[1];
-     var x1 = currentUser.get(coords_property)[0];
-     var y1=  currentUser.get(coords_property)[1];
+    var k2= rider.attributes.work_latlong[0];
+    var j2= rider.attributes.work_latlong[1];
+    var k1= currentUser.attributes.work_latlong[0];
+    var j1= currentUser.attributes.work_latlong[1];
+    //  var x2 = rider.get(coords_property)[0];
+    //  var y2 = rider.get(coords_property)[1];
+    //  var x1 = currentUser.get(coords_property)[0];
+    //  var y1=  currentUser.get(coords_property)[1];
 
      var deg2rad = function(deg) {
        rad = deg * Math.PI/180; // radians = degrees * pi/180
@@ -45,6 +55,9 @@
 
     var dlat= deg2rad(x2) - deg2rad(x1);
     var dlon= deg2rad(y2) - deg2rad(y1);
+
+    var dlat1= deg2rad(k2) - deg2rad(k1);
+    var dlon1= deg2rad(j2) - deg2rad(j1);
 
     var round = function(x) {
           return Math.round( x * 1000) / 1000;
@@ -58,11 +71,25 @@
 
     mi = round(dm);
     km = round(dk);
-    
+
+    // here's the heavy lifting
+    a1  = Math.pow(Math.sin(dlat1/2),2) + Math.cos(k1) * Math.cos(j1) * Math.pow(Math.sin(dlon1/2),2);
+    c1  = 2 * Math.atan2(Math.sqrt(a1),Math.sqrt(1-a1)); // great circle distance in radians
+    dm1 = c1 * Rm; // great circle distance in miles
+    dk1 = c1 * Rk; // great circle distance in km
+
+
+    mi1 = round(dm1);
+    km1 = round(dk1);
+
+
+
     //console.log(rider.attributes.user.id + ' is the user ID ');
     return { username: rider.get('name'),
              miles: mi,
+             work_miles: mi1,
              kilometers: km,
+             work_kilometers: km1,
              work: rider.get('work_address'),
              userId: rider.attributes.user.id,
              objectId: rider.id,
--- app/scripts/views/ride_calc_view.js
+++ app/scripts/views/ride_calc_view.js
@@ -19,17 +19,19 @@
     matchesCalc: function(){
 
       var results = _.map(collection, function(other) {
-        return currentUser.findDistance(other, 'home');
+        return currentUser.findDistance(other);
       });
 
-       var results2 = _.map(collection, function(other) {
-        return currentUser.findDistance(other, 'work');
-       });
+      //  var results2 = _.map(collection, function(other) {
+      //   return currentUser.findDistance(other, 'work');
+      //  });
 
-
+      console.log(results);
 
       function isBigEnough(element) {
-             return element.miles >  0 && element.miles < 5;
+             return element.miles >  0 && element.miles < 15
+                    && element.work_miles > 0 && element.work_miles < 15 ;
+
           }
       var homefilter = results.filter(isBigEnough);
 
@@ -37,14 +39,14 @@
 
 
       var neighbors = _.each(homefilter, function(x) {
-        $('.matchresults').append("<li class='matcher'>" + "<a href='"+ '#/allriders/' + x.objectId +"' >"  + "<img class='matchpic' src='" + x.picture + "'/>" + x.username + ' house ' + ' is ' + x.miles + ' miles away' +"</a>" + "</li>");
+        $('.matchresults').append("<li class='matcher'>" + "<a href='"+ '#/allriders/' + x.objectId +"' >"  + "<img class='matchpic' src='" + x.picture + "'/>" + x.username + ' house ' + ' is ' + x.miles + ' miles away' + x.work_miles + 'work mi away' + "</a>" + "</li>");
       });
 
     //  console.log('I work at ' + currentUser.attributes.work_address);
-      var neighbors2 = _.each(results2, function(x) {
+    //  var neighbors2 = _.each(results2, function(x) {
     //    console.log(x.username + ' work ' + ' is ' + x.miles + ' miles away');
         // $('.matchresults4').append("<li>" + x.username + ' work ' + ' is ' + x.miles + ' miles away' + "</li>");
-      });
+    //  });
 
 
 
